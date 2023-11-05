a4c8ba5a3b4505d339a86f3f0e6b078bb856239e
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-11-06 00:21:03 +0600

    modified the login and resiter page with redirect to each other page

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,62 +1,72 @@
 import { FcGoogle } from "react-icons/fc";
 import { NavLink } from "react-router-dom";
+import login from "../assets/image/login.jpg";
 
 const Login = () => {
   return (
-    <div className="min-h-screen bg-base-200 flex justify-center items-center">
-      <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
-        <form
-          className="card-body"
-          // onSubmit={handleSubmit}
-        >
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              onBlur={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-              onBlur={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <p className="text-center text-sm">
-            Don&apos;t have an account ?{" "}
-            <NavLink
-              to="/register"
-              className="text-primary font-bold hover:underline cursor-pointer "
-            >
-              Sign Up
-            </NavLink>
-          </p>
-          <div className="form-control mt-2">
-            <button type="submit" className="btn btn-primary">
-              Login
-            </button>
-          </div>
-          <div className="divider ">Or, Continue With</div>
-          <button
-            type="button"
-            // onClick={handleGoogleLogin}
-            className="btn btn-outline btn-primary  w-full flex justify-between items-center cursor-pointer "
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${login})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content flex-col text-center w-full">
+        <p className="text-5xl font-bold text-base-100">Log Now!</p>
+        <div className="shadow-lg w-1/3 rounded-lg bg-base-100">
+          <form
+            className="card-body"
+            // onSubmit={handleSubmit}
           >
-            Google
-            <FcGoogle className="w-8 h-8" />
-          </button>
-        </form>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                onBlur={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+                onBlur={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <p className="text-center text-black text-sm">
+              Don&apos;t have an account ?{" "}
+              <NavLink
+                to="/register"
+                className="text-primary font-bold hover:underline cursor-pointer "
+              >
+                Sign Up
+              </NavLink>
+            </p>
+            <div className="form-control mt-2">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+            <div className="divider text-black">Or, Continue With</div>
+            <button
+              type="button"
+              // onClick={handleGoogleLogin}
+              className="btn btn-outline btn-primary  w-full flex justify-between items-center cursor-pointer "
+            >
+              Google
+              <FcGoogle className="w-8 h-8" />
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -1,7 +1,87 @@
+import { NavLink } from "react-router-dom";
+import login from "../assets/image/login.jpg";
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${login})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content flex-col text-center w-full">
+        <p className="text-5xl font-bold text-base-100">Register Now!</p>
+        <div className="shadow-lg w-1/3 rounded-lg bg-base-100 ">
+          <form
+            //  onSubmit={handleSubmit}
+            className="card-body shadow-lg rounded-lg bg-base-100"
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+                onBlur={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="url"
+                placeholder="Photo URL"
+                className="input input-bordered"
+                required
+                onBlur={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+                onBlur={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+                onBlur={(e) => setPassword(e.target.value)}
+              />
+              <p className="text-center text-black text-sm pt-3">
+                Don&apos;t have an account ?{" "}
+                <NavLink
+                  to="/login"
+                  className="text-primary font-bold hover:underline cursor-pointer "
+                >
+                  Login
+                </NavLink>
+              </p>
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
